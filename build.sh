d11805d7ea29525ca09945e32dd4cbd711f3d6af
kaocher <kaocher82@gmail.com>
2021-01-05 04:36:52 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-git clone https://github.com/kaocher82/saleor-platform.git --recursive --jobs 3 & cd saleor-platform
+git clone https://github.com/kaocher82/saleor-platform.git --recursive --jobs 3
+cd saleor-platform
 # install docker compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
